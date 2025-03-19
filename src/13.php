8f402ea49f78616b69ea5ab8012a955aabbaaf84
singleyscallan680 <singleyscallan680@gmail.com>
2025-03-19 04:32:24 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,8 @@
+ <?php
+$number = rand(0,10); //Generate a random number between 0 and 10
+if ($number % 2 == 0){
+echo 'The number is even'; //If the number is even, display this message
+}else{
+echo 'The number is odd'; //If the number is odd, display this message
+}
+?>
