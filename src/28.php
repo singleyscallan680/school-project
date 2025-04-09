3d2173f70aead54029ab1dae27e1ee86efe74348
singleyscallan680 <singleyscallan680@gmail.com>
2025-04-09 10:48:25 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,13 @@
+<?php
+// Define variables
+$variable1 = "value1";
+$variable2 = "value2";
+
+// Check if both variables are defined
+if (isset($variable1) && isset($variable2)) {
+    // Code to be executed if both variables are defined
+}
+
+// Output the result
+echo "The two variables $variable1 and $variable2 have been checked.";
+?>
