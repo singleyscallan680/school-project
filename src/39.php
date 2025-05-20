da643c574baa612750adace037751240994b4df6
singleyscallan680 <singleyscallan680@gmail.com>
2025-05-20 04:26:40 +0000

    Add 39.php

---
+++ src/39.php
@@ -0,0 +1,11 @@
+<?php
+function echo_random_string($length = 10) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $random_string = '';
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $random_string;
+}
+echo_random_string();
+?>
