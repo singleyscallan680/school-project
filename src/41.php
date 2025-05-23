9327d3e52356e9f87893620801fea14b036a0f33
singleyscallan680 <singleyscallan680@gmail.com>
2025-05-23 05:52:16 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,12 @@
+<?php
+// Define variables and constants from the database or API call
+$var1 = "value1";
+$var2 = "value2";
+$var3 = "value3";
+
+// Create an array to hold the variables
+$vars_array = [ $var1, $var2, $var3 ];
+
+// Print the variable array
+print_r($vars_array);
+?>
